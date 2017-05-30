421cfe992454090eb4f0e019526a45afa77789a1
dustin.spears <dustin.spears@cgi.com>
2017-05-30 13:18:01 -0400

    script added

---
+++ centos7_vbox_guest_additions.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+yum update
+yum install -y dkms gcc make kernel-devel bzip2 binutils patch libgomp glibc-headers glibc-devel kernel-headers
+mkdir -p /media/cdrom
+mount /dev/cdrom /media/cdrom
+sh /media/cdrom/VBoxLinuxAdditions.run
