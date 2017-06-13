45008821c450691b15cbedc4c82e796f370b4abd
Dustin <dustin.spears@live.com>
2017-06-13 12:10:03 -0400

    Create centos7_vbox_guest_additions.sh

--- centos7_vbox_guest_additions.sh
+++ centos7_vbox_guest_additions.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-yum update
+yum update -y
 yum install -y dkms gcc make kernel-devel bzip2 binutils patch libgomp glibc-headers glibc-devel kernel-headers
 mkdir -p /media/cdrom
 mount /dev/cdrom /media/cdrom
