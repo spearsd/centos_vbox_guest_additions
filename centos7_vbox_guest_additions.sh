5234e30fdb26b34d753f8ec3505e5af7450672c7
Dustin <dustin.spears@live.com>
2017-06-13 12:44:34 -0400

    Create centos7_vbox_guest_additions.sh

--- centos7_vbox_guest_additions.sh
+++ centos7_vbox_guest_additions.sh
@@ -3,4 +3,6 @@ yum update -y
 yum install -y dkms gcc make kernel-devel bzip2 binutils patch libgomp glibc-headers glibc-devel kernel-headers
 mkdir -p /media/cdrom
 mount /dev/cdrom /media/cdrom
+# this step will fail, reboot and run the script again
+# once guest additions are installed, reboot again to show shared folders under /media
 sh /media/cdrom/VBoxLinuxAdditions.run
